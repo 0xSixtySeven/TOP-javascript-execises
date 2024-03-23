98c85bf98f93eba5f6b4b5e995ff6a725ebfa634
0xSixtySeven <sixtyseven.bbh@gmail.com>
2024-03-22 21:32:41 -0500

    Done with the 4th exercise, moving to the 3 remaining

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    let string = "";
+    for (let i = 0; i < times; i++) {
+        string += word;  // string = string + word  // string = "hey" + "hey" + "hey"  
+    }
+    if (times < 0) return "ERROR";
+    return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+// const prompt = require("prompt-sync")();
+
+// let word = prompt("Enter the word/words you want to reverse: ")
+
+const reverseString = function (word) {
+    return word.split("").reverse().join("");
+    // There is no direct way of reversing a word so we need to apply 3 steps: String.split > .reverse > .join.
+    // This will split the word in letters, reverse the orden and join the letters to a word again.
 
 };
 
