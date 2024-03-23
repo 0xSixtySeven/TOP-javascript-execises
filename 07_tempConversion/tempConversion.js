d804cef6d1e73f6fd352eec81b0aec9ad71c5637
0xSixtySeven <sixtyseven.bbh@gmail.com>
2024-03-22 23:17:19 -0500

    finished the 7 excersises of Arrays and Loops

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
-};
+// const prompt = require("prompt-sync")();
 
-const convertToFahrenheit = function() {
+// let fahrenheit = prompt("Enter a temperature in Fahrenheit: ");
+const convertToCelsius = function (fahrenheit) {
+  // when we want to round to 1 decimal we use the Math.round() method
+  return Math.round((fahrenheit - 32) * 5 / 9 * 10) / 10;
 };
+console.log(convertToCelsius());
 
+// let celsius = prompt("Enter a temperature in Celsius: ");
+const convertToFahrenheit = function (celsius) {
+  return Math.round((celsius * 9 / 5 + 32) * 10) / 10;
+};
+console.log(convertToFahrenheit());
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
